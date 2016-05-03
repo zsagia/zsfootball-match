8c514168239fe358d31943010e8c9d8611bf36a5
Zsolt Szabo <zsolt.szabo@liferay.com>
2016-05-03 21:44:44 +0200

    Adds match.js to package.json

---
+++ src/all/match.js
@@ -0,0 +1,12 @@
+'use strict';
+
+import ZsFootballMatchjs from '../ZsFootballMatch.js';
+import ZsFootballMatchsoy from '../ZsFootballMatch.soy';
+import ZsFootballMatchsoyjs from '../ZsFootballMatch.soy.js';
+
+export default Model;
+export {
+	ZsFootballMatchjs,
+	ZsFootballMatchsoy,
+	ZsFootballMatchsoyjs
+};

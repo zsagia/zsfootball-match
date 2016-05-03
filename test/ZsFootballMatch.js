ae0dbe8e13014e7b71f908e0f2babd7d7c68f8a2
Zsolt Szabo <zsolt.szabo@liferay.com>
2016-05-03 15:13:43 +0200

    Adds an empty test class

---
+++ test/ZsFootballMatch.js
@@ -0,0 +1,9 @@
+'use strict';
+
+import ZsFootballMatch from '../src/ZsFootballMatch';
+
+describe('ZsFootballMatch', function() {
+	it('should be tested', function() {
+		assert.fail('No tests for this module yet.');
+	});
+});

3863b2f9abd582a4de519f848344f961d62cc040
Zsolt Szabo <zsolt.szabo@liferay.com>
2016-05-11 19:20:24 +0200

    Removes configs

--- gulpfile.js
+++ gulpfile.js
@@ -5,9 +5,5 @@ var metal = require('gulp-metal');
 metal.registerTasks({
 	bundleCssFileName: 'zsFootballMatch.css',
 	bundleFileName: 'zsFootballMatch.js',
-	moduleName: 'zsfootball-match',
-	testNodeSrc: [
-		'env/test/node.js',
-		'test/**/*.js'
-	]
+	moduleName: 'zsfootball-match'
 });

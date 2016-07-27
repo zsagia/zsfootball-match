23c8da74b3c01f10b4ce4e7cdec119cf7917cebf
Zsolt Szabo <zsolt.szabo@liferay.com>
2016-07-27 12:15:08 +0200

    Added "_renderSimpleView" template

--- src/Match.soy.js
+++ src/Match.soy.js
@@ -46,7 +46,9 @@ function $render(opt_data, opt_ignored, opt_ijData) {
       $renderRowView_(opt_data, null, opt_ijData);
       break;
     case 1:
-      $renderRowView_(opt_data, null, opt_ijData);
+      break;
+    case 2:
+      $renderSimpleView_(opt_data, null, opt_ijData);
       break;
   }
 }
@@ -67,7 +69,7 @@ function $renderRowView_(opt_data, opt_ignored, opt_ijData) {
   ie_open('table', null, null,
       'class', 'zsfootball-match' + (opt_data.elementClasses ? ' ' + opt_data.elementClasses : ''),
       'data-onclick', 'onRowClickHandler');
-    var goals__soy20 = opt_data.match.goals;
+    var goals__soy19 = opt_data.match.goals;
     ie_open('tr', null, null,
         'class', 'zsfootball-match-row');
       ie_open('td', null, null,
@@ -81,9 +83,9 @@ function $renderRowView_(opt_data, opt_ignored, opt_ijData) {
       ie_open('td', null, null,
           'class', 'result');
         ie_open('span');
-          itext((goog.asserts.assert((goals__soy20.homeGoals.firstHalf.length + goals__soy20.homeGoals.secondHalf.length) != null), goals__soy20.homeGoals.firstHalf.length + goals__soy20.homeGoals.secondHalf.length));
+          itext((goog.asserts.assert((goals__soy19.homeGoals.firstHalf.length + goals__soy19.homeGoals.secondHalf.length) != null), goals__soy19.homeGoals.firstHalf.length + goals__soy19.homeGoals.secondHalf.length));
           itext(' - ');
-          itext((goog.asserts.assert((goals__soy20.awayGoals.firstHalf.length + goals__soy20.awayGoals.secondHalf.length) != null), goals__soy20.awayGoals.firstHalf.length + goals__soy20.awayGoals.secondHalf.length));
+          itext((goog.asserts.assert((goals__soy19.awayGoals.firstHalf.length + goals__soy19.awayGoals.secondHalf.length) != null), goals__soy19.awayGoals.firstHalf.length + goals__soy19.awayGoals.secondHalf.length));
         ie_close('span');
       ie_close('td');
       ie_open('td', null, null,
@@ -107,6 +109,48 @@ function $renderRowView_(opt_data, opt_ignored, opt_ijData) {
 exports.renderRowView_ = $renderRowView_;
 if (goog.DEBUG) {
   $renderRowView_.soyTemplateName = 'Match.renderRowView_';
+}
+
+
+/**
+ * @param {Object<string, *>=} opt_data
+ * @param {(null|undefined)=} opt_ignored
+ * @param {Object<string, *>=} opt_ijData
+ * @return {void}
+ * @suppress {checkTypes}
+ */
+function $renderSimpleView_(opt_data, opt_ignored, opt_ijData) {
+  ie_open('table', null, null,
+      'class', 'zsfootball-match' + (opt_data.elementClasses ? ' ' + opt_data.elementClasses : ''));
+    var goals__soy41 = opt_data.match.goals;
+    ie_open('tr', null, null,
+        'class', 'zsfootball-match-row');
+      ie_open('td', null, null,
+          'class', 'home-club');
+        itext((goog.asserts.assert((opt_data.match.homeClub.title) != null), opt_data.match.homeClub.title));
+      ie_close('td');
+      ie_open('td', null, null,
+          'class', 'result');
+        ie_open('span');
+          if (goals__soy41) {
+            itext((goog.asserts.assert((goals__soy41.homeGoals.firstHalf + goals__soy41.homeGoals.secondHalf) != null), goals__soy41.homeGoals.firstHalf + goals__soy41.homeGoals.secondHalf));
+          }
+          itext('-');
+          if (goals__soy41) {
+            itext((goog.asserts.assert((goals__soy41.awayGoals.firstHalf + goals__soy41.awayGoals.secondHalf) != null), goals__soy41.awayGoals.firstHalf + goals__soy41.awayGoals.secondHalf));
+          }
+        ie_close('span');
+      ie_close('td');
+      ie_open('td', null, null,
+          'class', 'away-club');
+        itext((goog.asserts.assert((opt_data.match.awayClub.title) != null), opt_data.match.awayClub.title));
+      ie_close('td');
+    ie_close('tr');
+  ie_close('table');
+}
+exports.renderSimpleView_ = $renderSimpleView_;
+if (goog.DEBUG) {
+  $renderSimpleView_.soyTemplateName = 'Match.renderSimpleView_';
 }
 
 
@@ -146,7 +190,7 @@ function $renderDetails_(opt_data, opt_ignored, opt_ijData) {
         ie_close('table');
       ie_close('td');
     ie_close('tr');
-    var goals__soy48 = opt_data.match.goals;
+    var goals__soy65 = opt_data.match.goals;
     ie_open('tr', null, null,
         'class', 'first-half half');
       ie_open('td', null, null,
@@ -154,7 +198,7 @@ function $renderDetails_(opt_data, opt_ignored, opt_ijData) {
         itext('1. half');
       ie_close('td');
     ie_close('tr');
-    $renderGoals_({halfGoals: opt_data.match.firstHalfGoals, homeGoals: goals__soy48.homeGoals.firstHalf.length, awayGoals: goals__soy48.awayGoals.firstHalf.length}, null, opt_ijData);
+    $renderGoals_({halfGoals: opt_data.match.firstHalfGoals, homeGoals: goals__soy65.homeGoals.firstHalf.length, awayGoals: goals__soy65.awayGoals.firstHalf.length}, null, opt_ijData);
     ie_open('tr', null, null,
         'class', 'second-half half');
       ie_open('td', null, null,
@@ -162,7 +206,7 @@ function $renderDetails_(opt_data, opt_ignored, opt_ijData) {
         itext('2. half');
       ie_close('td');
     ie_close('tr');
-    $renderGoals_({halfGoals: opt_data.match.secondHalfGoals, homeGoals: goals__soy48.homeGoals.secondHalf.length, awayGoals: goals__soy48.awayGoals.secondHalf.length}, null, opt_ijData);
+    $renderGoals_({halfGoals: opt_data.match.secondHalfGoals, homeGoals: goals__soy65.homeGoals.secondHalf.length, awayGoals: goals__soy65.awayGoals.secondHalf.length}, null, opt_ijData);
   ie_close('table');
 }
 exports.renderDetails_ = $renderDetails_;
@@ -179,13 +223,16 @@ if (goog.DEBUG) {
  * @suppress {checkTypes}
  */
 function $renderGoals_(opt_data, opt_ignored, opt_ijData) {
-  var rowLength__soy61 = opt_data.halfGoals.length;
-  var goalList69 = opt_data.halfGoals;
-  var goalListLen69 = goalList69.length;
-  for (var goalIndex69 = 0; goalIndex69 < goalListLen69; goalIndex69++) {
-    var goalData69 = goalList69[goalIndex69];
-    var columnIndex__soy62 = goalIndex69;
-    $renderGoal_({columnIndex: columnIndex__soy62, goal: goalData69, homeGoals: opt_data.homeGoals, awayGoals: opt_data.awayGoals, rowLength: rowLength__soy61}, null, opt_ijData);
+  var rowLength__soy78 = opt_data.halfGoals.length;
+  if (opt_data.halfGoals.length == 0) {
+    $renderGoal_({columnIndex: 0, homeGoals: 0, awayGoals: 0, rowLength: rowLength__soy78}, null, opt_ijData);
+  }
+  var goalList93 = opt_data.halfGoals;
+  var goalListLen93 = goalList93.length;
+  for (var goalIndex93 = 0; goalIndex93 < goalListLen93; goalIndex93++) {
+    var goalData93 = goalList93[goalIndex93];
+    var columnIndex__soy86 = goalIndex93;
+    $renderGoal_({columnIndex: columnIndex__soy86, goal: goalData93, homeGoals: opt_data.homeGoals, awayGoals: opt_data.awayGoals, rowLength: rowLength__soy78}, null, opt_ijData);
   }
 }
 exports.renderGoals_ = $renderGoals_;
@@ -202,10 +249,12 @@ if (goog.DEBUG) {
  * @suppress {checkTypes}
  */
 function $renderGoal_(opt_data, opt_ignored, opt_ijData) {
-  ie_open('tr');
+  ie_open('tr', null, null,
+      'class', 'goals');
     ie_open('td', null, null,
+        'class', 'home-goal-scoorer',
         'colspan', '2');
-      if (opt_data.goal.goalType == 'homeGoal') {
+      if (opt_data.homeGoals > 0 && opt_data.goal.goalType == 'homeGoal') {
         itext((goog.asserts.assert((opt_data.goal.time) != null), opt_data.goal.time));
         itext('\' ');
         itext((goog.asserts.assert((opt_data.goal.player.fullName) != null), opt_data.goal.player.fullName));
@@ -213,6 +262,7 @@ function $renderGoal_(opt_data, opt_ignored, opt_ijData) {
     ie_close('td');
     if (opt_data.columnIndex == 0) {
       ie_open('td', null, null,
+          'class', 'result',
           'rowspan', opt_data.rowLength);
         itext((goog.asserts.assert((opt_data.homeGoals) != null), opt_data.homeGoals));
         itext(' - ');
@@ -221,8 +271,9 @@ function $renderGoal_(opt_data, opt_ignored, opt_ijData) {
       ie_close('td');
     }
     ie_open('td', null, null,
+        'class', 'away-goal-scoorer',
         'colspan', '2');
-      if (opt_data.goal.goalType == 'awayGoal') {
+      if (opt_data.awayGoals > 0 && opt_data.goal.goalType == 'awayGoal') {
         itext((goog.asserts.assert((opt_data.goal.time) != null), opt_data.goal.time));
         itext('\' ');
         itext((goog.asserts.assert((opt_data.goal.player.fullName) != null), opt_data.goal.player.fullName));
@@ -239,6 +290,8 @@ exports.render.params = ["match","localeDate","localHourMinute","viewType","elem
 exports.render.types = {"match":"any","localeDate":"any","localHourMinute":"any","viewType":"any","elementClasses":"any"};
 exports.renderRowView_.params = ["elementClasses","localeDate","localHourMinute","match"];
 exports.renderRowView_.types = {"elementClasses":"any","localeDate":"any","localHourMinute":"any","match":"any"};
+exports.renderSimpleView_.params = ["elementClasses","match"];
+exports.renderSimpleView_.types = {"elementClasses":"any","match":"any"};
 exports.renderDetails_.params = ["localeDate","match"];
 exports.renderDetails_.types = {"localeDate":"any","match":"any"};
 exports.renderGoals_.params = ["halfGoals","awayGoals","homeGoals"];
